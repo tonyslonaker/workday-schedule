6f5d677c380c2a98292a935b0cafe890904f66ec
Tony Slonaker <tony.slonaker@gmail.com>
2021-08-28 11:15:10 -0600

    update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Get current day using moment.js
+var currentDay = moment().format("dddd, MMMM Do");
+
+// Set each timeblock in the daily schedule using moment.js
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment().hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment().hour(13);
+var hour14 = moment().hour(14);
+var hour15 = moment().hour(15);
+var hour16 = moment().hour(16);
+var hour17 = moment().hour(17);
+var hour18 = moment().hour(18);
+
